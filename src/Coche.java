a443574a9e9c67822d1b716381180c21c3b9fc85
Abraham <abraham.belen.duran@gmail.com>
2023-03-28 15:17:55 +0200

    Refactorizado

--- src/Coche.java
+++ src/Coche.java
@@ -1,11 +1,11 @@
 public class Coche {
     public int velocidad;
 
-    public void acelerar(int aceleracion) {
+    public void acelerar_Abraham_belen_Duran(int aceleracion) {
         velocidad = velocidad + aceleracion;
     }
 
-    public void decelerar(int deceleracion) {
+    public void decelerar_Abraham_Belen_Duran(int deceleracion) {
         velocidad = velocidad - deceleracion;
         if(velocidad<0){
             velocidad = 0;
